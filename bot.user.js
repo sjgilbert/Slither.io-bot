a9caca4a68b644bdba0617cc635bfb49b8199de1
Sam Gilbert <samjgilbert94@gmail.com>
2016-11-10 11:57:52 -0600

    trying CORS

--- bot.user.js
+++ bot.user.js
@@ -22,6 +22,50 @@ Uncomment variables you wish to change from their default values
 Changes you make here will be kept between script versions
 */
 
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+var url = "https://www.google.com";
+
+var xhr = createCORSRequest('GET', url);
+if (!xhr) {
+  throw new Error('CORS not supported');
+}
+
+xhr.onload = function() {
+ var responseText = xhr.responseText;
+ console.log(responseText);
+ // process the response.
+};
+
+xhr.onerror = function() {
+  console.log('There was an error!');
+};
+
+xhr.send();
+
+
 var customBotOptions = {
     // target fps
     // targetFps: 30,
